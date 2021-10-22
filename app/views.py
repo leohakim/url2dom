1077a88146193d3e026f82738b28d89aa35fc41f
Leohakim <leohakim@gmail.com>
2021-10-22 20:12:55 +0200

    Change to POST method. Allow Array of Xpath. Return Individual Exceptions

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 import datetime
 
 import validators
-from flask import request
+from flask import request, jsonify
 from selenium import webdriver
-from selenium.common.exceptions import InvalidSelectorException
+from selenium.common.exceptions import InvalidSelectorException, NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 
 from app import app
@@ -19,27 +19,46 @@ opts.add_argument("--disable-dev-shm-usage")
 opts.add_argument("--disable-gpu")
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def home():
+    url = request.json.get('url')
+    xpath = request.json.get('xpath')
+    _datetime = datetime.datetime.now().isoformat()
+    if not url or not validators.url(url):
+        return {
+                   "datetime": _datetime,
+                   "status": "Error",
+                   "error": "URL not valid"
+               }, 400
     with webdriver.Chrome(options=opts, executable_path=executable) as browser:
-        url = request.args.get('url')
-        xpath = request.args.get('xpath')
-        if not url or not validators.url(url):
-            return "URL not valid", 400
-        browser.get(url)
+        try:
+            browser.get(url)
+        except WebDriverException as e:
+            return {
+                       "datetime": _datetime,
+                       "status": "Error",
+                       "error": f"{e}"
+                   }, 400
         if not xpath:
             return {
-                "datetime": datetime.datetime.now(),
-                "data": browser.page_source,
+                "datetime": _datetime,
                 "status": "Ok",
+                "data": browser.page_source,
             }
-        if xpath:
+        data = dict()
+        all_failed = True
+        for element in xpath:
             try:
-                data = browser.find_element_by_xpath(f"{xpath}")
-                if data:
-                    return {
-                        "status": "Okaaa",
-                        "data": data.text
-                    }
-            except InvalidSelectorException as e:
-                return f"{e}", 400
+                value = browser.find_element_by_xpath(f"{element}").text
+                all_failed = False
+            except (InvalidSelectorException, NoSuchElementException) as e:
+                value = f"{e}"
+            data.update({element: value})
+
+        retorno = {
+            "datetime": _datetime,
+            "status": "Error" if all_failed else "Ok",
+            "data": data
+        }
+
+        return jsonify(retorno)
--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:80 --reload
+gunicorn --chdir app app:app -w 2 --threads 2 --timeout 120 -b 0.0.0.0:80 --reload
